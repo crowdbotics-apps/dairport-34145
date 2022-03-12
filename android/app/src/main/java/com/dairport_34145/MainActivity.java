bdda11d6fe89016ad2e4382a4ad7f14526211d28
Crowdbotics <team@crowdbotics.com>
2022-03-12 20:35:01 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dairport_34145/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dairport_34145;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dairport_34145";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "dairport_34145",
+  url: "https://dairport_34145.botics.co",
+  api: "https://dairport_34145.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
